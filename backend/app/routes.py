ee4eefdc4a28e0fd0eaeece6e4c1eb392a31bf39
Joao vicente <joaorovera73@gmail.com>
2024-10-10 19:29:04 -0300

    Fix routes.py to acess index.html

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,12 +1,12 @@
 from .controller import process_ticket
-
-def route_teste(app):
-    @app.route("/teste")
-    def teste():
-        return "<<<TESTANDO>>>"
+from flask import render_template
 
 def config_routes(app):
     @app.route("/ticket", methods=["POST"])
     def ticket_process():
         return process_ticket
+    
+    @app.route("/")
+    def home():
+        return render_template("index.html")
     
