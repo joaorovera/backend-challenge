d23f4990c73219b03a54d73fa5e41e467cf2af17
Joao vicente <joaorovera73@gmail.com>
2024-10-10 14:05:26 -0300

    Create validators.py to verify the infos

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,20 +1,27 @@
+import os
 from flask import Flask
 from flask_mail import Mail
+from flask_cors import CORS
 from dotenv import load_dotenv
-import os
 
 email = Mail()
 
 def create_app():
+    
+    CORS(app)
     app = Flask(__name__)
     load_dotenv() #carregar variáveis de ambiente.
 
-    app.config["MAIL_SERVER"] = os.getenv("MAIL_HOST")
-    app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT"))
-    app.config["MAIL_USE_TLS"] = not os.getenv("MAIL_SECURE").lower() == True
+
+
     app.config["MAIL_USERNAME"] = os.getenv("MAIL_AUTH_USER")
     app.config["MAIL_PASSWORD"] = os.getenv("MAIL_AUTH_PASS")
     app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_AUTH_USER")
+    app.config["MAIL_SERVER"] = os.getenv("MAIL_HOST")
+    app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT"))
+    app.config["MAIL_USE_TLS"] = not os.getenv("MAIL_SECURE").lower() == True
+
+
 
     Mail.init_app(app)
 
---
+++ backend/app/validators.py
@@ -0,0 +1,61 @@
+import os
+from flask_mail import Mail
+from flask_mailman import EmailMessage
+import re
+import requests
+
+email = Mail()
+
+def check_email(content):
+    standard = r"[^@]+@[^@]+\.[^@]+"
+    mail = content.get("mail")
+    return not re.match(standard, mail)
+
+def check_name(content):
+    name = content.get("name")
+    if len(name) >= 3:
+        return False
+    return True
+
+def check_message(content):
+    msg = content.get("comment")
+    return not (10<= len(msg) <= 2000)
+
+def check_captcha(content):
+    if "g-recaptcha-response" in content:
+        captcha = content["g-recaptcha-response"]
+        if valid_token(captcha):
+            return False
+    return True
+
+
+def valid_token(token):
+    url = os.getenv("RECAPTCHA_URL")
+    secret = os.getenv("RECAPTCHA_SECRET")
+    payload = {"secret": secret,
+               "response": token
+    }
+    response = requests.post(url, data=payload)
+    result = response.json()
+    if "success" in result:
+        resultado = result["success"]
+        if resultado == True:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def send_email(content):
+    msg = EmailMessage(
+    subject= os.getenv("TEXT_MAIL_TITLE"),
+    body= os.getenv("TEXT_MAIL_BODY").format(name=content["name"], email=content["mail"], comment=content["comment"]),
+    sender= os.getenv("MAIL_AUTH_USER"),
+    receivers = [os.getenv("MAIL_AUTH_USER")],
+    )
+    try:
+        msg.send()
+        return "Email enviado"
+    except Exception as e:
+        print(e)
+        return True
